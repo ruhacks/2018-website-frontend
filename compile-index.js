1b3c8d00354b3a44c2f6a025d4228392a5f0c31b
ktsuench <will289@hotmail.com>
2017-11-20 02:22:36 -0500

    inital setup

--- compile-index.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const pug = require('pug');
-const input = './views/index.pug';
-const output = './index.html';
-const options = { pretty: true };
-
-fs.writeFileSync(output, pug.renderFile(input, options));
