1cbce7e8e4b2b0bbaae51f999352eb1fd42bc91e
ktsuench <will289@hotmail.com>
2017-11-19 22:23:16 -0500

    cleaned up files

--- 2018/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  url: process.env.DATABASE_URL_2018 || null,
-  default_url: 'mongodb://localhost:27017/test',
-};
--- 2018/config/test.js
+++
@@ -1,5 +0,0 @@
-const dotenv = require('dotenv');
-
-if (!Object.prototype.hasOwnProperty.call(process.env, 'REQUIRE_DOT_ENV') || process.env.REQUIRE_DOT_ENV === 'true') {
-  dotenv.config({ path: './2018/.env' });
-}
