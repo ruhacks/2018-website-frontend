ae1670891fedcfe64c8d5a965ce1558932920391
streacy <streacy@ryerson.ca>
2018-03-29 15:49:42 -0400

    added workshops and locations

--- scripts/script.js
+++ scripts/script.js
@@ -6,36 +6,50 @@ google.charts.load("current", {packages:["timeline"]});
     var chart = new google.visualization.Timeline(container);
     var dataTable = new google.visualization.DataTable();
 
-    dataTable.addColumn({ type: 'string', id: 'Room' });
+    dataTable.addColumn({ type: 'string', id: 'Category' });
     dataTable.addColumn({ type: 'string', id: 'Name' });
+    dataTable.addColumn({ type: 'string', role: 'des'});
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-      [ 'Main Events',  'Early Registration',    new Date(2018,5,25,17,00), new Date(2018,5,25,18,00) ],
-      [ 'Main Events',  'Registration',    new Date(2018,5,25,18,00), new Date(2018,5,25,23,00) ],
-      [ 'Events',  'Hacking Begins',    new Date(2018,5,25,22,00), new Date(2018,5,26,00,00) ],
-      [ 'Events',  'Opening Ceremony',    new Date(2018,5,25,19,00), new Date(2018,5,25,20,30) ],
-      [ 'Events',  'Team Formation',    new Date(2018,5,25,21,00), new Date(2018,5,25,22,00) ],
-      [ 'Food',  'Dinner',    new Date(2018,5,25,20,30), new Date(2018,5,25,23,30) ],
-      [ 'Activities',  'Hacker-Sponsor Mingle', new Date(2018,5,25,17,00), new Date(2018,5,25,19,00) ],
-      [ 'Workshops', 'Workshop #1',   new Date(2018,5,25,23,00), new Date(2018,5,26,0,00) ],
+      [ 'Main Events',  'Early Registration',null,    new Date(2018,5,25,17,00), new Date(2018,5,25,18,00) ],
+      [ 'Main Events',  'Registration',null,    new Date(2018,5,25,18,00), new Date(2018,5,25,23,00) ],
+      [ 'Food',  'Dinner', 'tooltip',   new Date(2018,5,25,20,30), new Date(2018,5,25,23,30) ],
+      [ 'Activities',  'Hacker-Sponsor Mingle',null, new Date(2018,5,25,17,00), new Date(2018,5,25,19,00) ],
+      [ 'Workshops', 'Workshop #1',  null, new Date(2018,5,25,23,00), new Date(2018,5,26,0,00) ],
+      [ 'Workshops', 'AI + Android Workshop',  null, new Date(2018,5,26,18,30), new Date(2018,5,26,19,30) ],
+      [ 'Food',  'Midnight Snack',  null,  new Date(2018,5,26,00,00), new Date(2018,5,26,2,00) ],
+      [ 'Food',  'Breakfast',  null,  new Date(2018,5,26,7,00), new Date(2018,5,26,9,00) ], 
+      [ 'Food',  'Lunch', null,   new Date(2018,5,26,13,00), new Date(2018,5,26,15,00) ], 
+      [ 'Food',  'Dinner',  null,  new Date(2018,5,26,19,00), new Date(2018,5,26,21,30) ], 
+      [ 'Food',  'Midnight Snack', null,   new Date(2018,5,27,00,00), new Date(2018,5,27,2,00) ],
+      [ 'Food',  'Brunch',  null,  new Date(2018,5,27,9,00), new Date(2018,5,27,12,00) ],
+      [ 'Main Events',  'Room Allocation for Demos',  null,  new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ],
+      [ 'Main Events',  'Project Demos to Judges',  null,  new Date(2018,5,27,10,00), new Date(2018,5,27,12,00) ],      
+      [ 'Main Events',   'CLosing Ceromony','Ryerson Theatre',    new Date(2018,5,27,13,00), new Date(2018,5,27,16,00) ]]);
+    dataTable.addRows([
+      [ 'Main Events',  'Hacking Begins',  null,  new Date(2018,5,25,22,00), new Date(2018,5,26,00,00) ],
+      [ 'Main Events',  'Opening Ceremony', 'Ryerson Theatre',   new Date(2018,5,25,19,00), new Date(2018,5,25,20,30) ],
+      [ 'Main Events',  'Team Formation', null,   new Date(2018,5,25,21,00), new Date(2018,5,25,22,00) ],
+      [ 'Main Events',  'Project Submission', null,   new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ]]);
 
-      [ 'Food',  'Midnight Snack',    new Date(2018,5,26,00,00), new Date(2018,5,26,2,00) ],
-      [ 'Food',  'Breakfast',    new Date(2018,5,26,7,00), new Date(2018,5,26,9,00) ], 
-      [ 'Food',  'Lunch',    new Date(2018,5,26,13,00), new Date(2018,5,26,15,00) ], 
-      [ 'Food',  'Dinner',    new Date(2018,5,26,19,00), new Date(2018,5,26,21,30) ], 
-
-      [ 'Food',  'Midnight Snack',    new Date(2018,5,27,00,00), new Date(2018,5,27,2,00) ],
-      [ 'Events',  'Project Submission',    new Date(2018,5,27,8,00), new Date(2018,5,27,9,00) ],
-      [ 'Food',  'Brunch',    new Date(2018,5,27,9,00), new Date(2018,5,27,12,00) ],
-      [ 'Main Events',  'Room Allocation for Demos',    new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ],
-      [ 'Main Events',  'Project Demos to Judges',    new Date(2018,5,27,10,00), new Date(2018,5,27,12,00) ],      
-      [ 'Main Events',   'CLosing Ceromony',           new Date(2018,5,27,13,00), new Date(2018,5,27,16,00) ]]);
-
-    var options = { timeline: { colorByRowLabel: true }, width: 6000, height: 300,
+    var options = { timeline: { colorByRowLabel: true}, width: 5000, height: 300, barLabelStyle: {
+        fontSize: 40}
     };
 
     chart.draw(dataTable, options);
-  }
 
-  // colorByRowLabel: true
+    google.visualization.events.addListener(chart, 'onmouseover', function(e) {
+        setTooltipContent(dataTable,e.row);
+    });
+}
+
+
+function setTooltipContent(dataTable,row) {
+    var duration = Math.abs(dataTable.getValue(row, 4) - dataTable.getValue(row, 3))/ 36e5;
+    if (row != null) {
+        var content = '<br><div class="custom-tooltip"><b>' +dataTable.getValue(row, 1)+ '<br><br>Location:</b> ' + dataTable.getValue(row, 2) +'<br>'+'<b>Duration:</b> '+ duration+ ' Hours'+ '</div>'; //generate tooltip content
+        var tooltip = document.getElementsByClassName("google-visualization-tooltip")[0];
+        tooltip.innerHTML = content;
+    }
+}
