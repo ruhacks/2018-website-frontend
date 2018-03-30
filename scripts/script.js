374ea4f26a1e66c033940c66954e6e4ec34f3588
ktsuench <will289@hotmail.com>
2018-03-30 16:38:45 -0400

    updated null locations to 'TBD'

--- scripts/script.js
+++ scripts/script.js
@@ -12,26 +12,26 @@ google.charts.load("current", {packages:["timeline"]});
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-      [ 'Main Events',  'Early Registration',null,    new Date(2018,5,25,17,00), new Date(2018,5,25,18,00) ],
-      [ 'Main Events',  'Registration',null,    new Date(2018,5,25,18,00), new Date(2018,5,25,23,00) ],
-      [ 'Food',  'Dinner', 'tooltip',   new Date(2018,5,25,20,30), new Date(2018,5,25,23,30) ],
-      [ 'Activities',  'Hacker-Sponsor Mingle',null, new Date(2018,5,25,17,00), new Date(2018,5,25,19,00) ],
-      [ 'Workshops', 'Workshop #1',  null, new Date(2018,5,25,23,00), new Date(2018,5,26,0,00) ],
-      [ 'Workshops', 'AI + Android Workshop',  null, new Date(2018,5,26,18,30), new Date(2018,5,26,19,30) ],
-      [ 'Food',  'Midnight Snack',  null,  new Date(2018,5,26,00,00), new Date(2018,5,26,2,00) ],
-      [ 'Food',  'Breakfast',  null,  new Date(2018,5,26,7,00), new Date(2018,5,26,9,00) ], 
-      [ 'Food',  'Lunch', null,   new Date(2018,5,26,13,00), new Date(2018,5,26,15,00) ], 
-      [ 'Food',  'Dinner',  null,  new Date(2018,5,26,19,00), new Date(2018,5,26,21,30) ], 
-      [ 'Food',  'Midnight Snack', null,   new Date(2018,5,27,00,00), new Date(2018,5,27,2,00) ],
-      [ 'Food',  'Brunch',  null,  new Date(2018,5,27,9,00), new Date(2018,5,27,12,00) ],
-      [ 'Main Events',  'Room Allocation for Demos',  null,  new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ],
-      [ 'Main Events',  'Project Demos to Judges',  null,  new Date(2018,5,27,10,00), new Date(2018,5,27,12,00) ],      
-      [ 'Main Events',   'Closing Ceromony','Ryerson Theatre',    new Date(2018,5,27,13,00), new Date(2018,5,27,16,00) ]]);
+      [ 'Main Events', 'Early Registration', 'TDB', new Date(2018,5,25,17,00), new Date(2018,5,25,18,00) ],
+      [ 'Main Events', 'Registration', 'TDB', new Date(2018,5,25,18,00), new Date(2018,5,25,23,00) ],
+      [ 'Food', 'Dinner', 'TDB', new Date(2018,5,25,20,30), new Date(2018,5,25,23,30) ],
+      [ 'Activities', 'Hacker-Sponsor Mingle', 'TDB', new Date(2018,5,25,17,00), new Date(2018,5,25,19,00) ],
+      [ 'Workshops', 'Workshop #1', 'TDB', new Date(2018,5,25,23,00), new Date(2018,5,26,0,00) ],
+      [ 'Workshops', 'AI + Android Workshop', 'TDB', new Date(2018,5,26,18,30), new Date(2018,5,26,19,30) ],
+      [ 'Food', 'Midnight Snack', 'TDB', new Date(2018,5,26,00,00), new Date(2018,5,26,2,00) ],
+      [ 'Food', 'Breakfast', 'TDB', new Date(2018,5,26,7,00), new Date(2018,5,26,9,00) ], 
+      [ 'Food','Lunch', 'TDB', new Date(2018,5,26,13,00), new Date(2018,5,26,15,00) ], 
+      [ 'Food', 'Dinner',  'TDB', new Date(2018,5,26,19,00), new Date(2018,5,26,21,30) ], 
+      [ 'Food', 'Midnight Snack', 'TDB', new Date(2018,5,27,00,00), new Date(2018,5,27,2,00) ],
+      [ 'Food', 'Brunch', 'TDB', new Date(2018,5,27,9,00), new Date(2018,5,27,12,00) ],
+      [ 'Main Events', 'Room Allocation for Demos', 'TDB', new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ],
+      [ 'Main Events', 'Project Demos to Judges', 'TDB', new Date(2018,5,27,10,00), new Date(2018,5,27,12,00) ],      
+      [ 'Main Events', 'Closing Ceromony','Ryerson Theatre', new Date(2018,5,27,13,00), new Date(2018,5,27,16,00) ]]);
     dataTable.addRows([
-      [ 'Main Events',  'Hacking Begins',  null,  new Date(2018,5,25,22,00), new Date(2018,5,26,00,00) ],
-      [ 'Main Events',  'Opening Ceremony', 'Ryerson Theatre',   new Date(2018,5,25,19,00), new Date(2018,5,25,20,30) ],
-      [ 'Main Events',  'Team Formation', null,   new Date(2018,5,25,21,00), new Date(2018,5,25,22,00) ],
-      [ 'Main Events',  'Project Submission', null,   new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ]]);
+      [ 'Main Events', 'Hacking Begins', 'TDB', new Date(2018,5,25,22,00), new Date(2018,5,26,00,00) ],
+      [ 'Main Events', 'Opening Ceremony', 'Ryerson Theatre', new Date(2018,5,25,19,00), new Date(2018,5,25,20,30) ],
+      [ 'Main Events', 'Team Formation', 'TDB', new Date(2018,5,25,21,00), new Date(2018,5,25,22,00) ],
+      [ 'Main Events', 'Project Submission', 'TDB', new Date(2018,5,27,8,00), new Date(2018,5,27,10,00) ]]);
 
     var options = { timeline: { colorByRowLabel: true}, width: 5000, height: 300, barLabelStyle: {
         fontSize: 80}
